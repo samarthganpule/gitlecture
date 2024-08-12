f27a1a2a4125f6329521e9b2b86a9902b8a07be0
samarthganpule <samarthganpule43@gmail.com>
2024-08-12 22:48:10 +0530

    first python

---
+++ python1.py
@@ -0,0 +1,36 @@
+# import pyttsx3
+# start = pyttsx3.init()
+# start.say("this is python")
+# start.runAndWait()/
+
+# a= "this posg for\n the database\tuu rr jnsfe"
+# b= "harry is an idiot boy bit\\ of love"
+# print(a)
+# print(b)
+
+# input = input("enter you name ")
+# print(f"Good night {input}" )
+# print(f"{input} This is am good DAY RIGHT {input}")
+
+# letter = '''Dear star <|name|> your collage <|collage|>'''
+# print(letter.replace("<|name|>","sami").replace("<|collage|>","ADYPU"))
+# letter1 = ''' this is kaushal parmar the star of class'''
+# print(letter1.replace("kaushal","sami"))
+# input1=input("enter your name")
+# print(input1)
+# print(input1.replace("hello","hey"))
+
+# from datetime import datetime
+# date = datetime.now()
+# let = ''' hello |name| good age today |age| day '''
+
+# inp= input("enter your name")
+# inpt = int(input("enter age"))
+
+
+# print(let.replace("|name|",inp).replace("|age|",str(inpt)).replace("day",str(date)),"the datetimre is",date)
+
+# num = 34
+# print(num.replace(34,43))
+# print(num.replace(34,43)) 
+
---
+++ python2.py
@@ -0,0 +1,9 @@
+probuct =["good","phone",34,55.5,True,"kaushal"]
+# print(probuct[4])
+# probuct[0]="bad"
+# print(probuct)
+print(probuct.remove("phone"))
+print(probuct.pop(3))
+print(probuct.pop(0))
+
+print(probuct)
